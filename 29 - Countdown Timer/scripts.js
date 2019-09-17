5abcebfec2af04c026cb30a0976a952a369ae02e
Laura Pastor <laurapastorj@gmail.com>
2019-09-17 11:35:09 +0200

    day29

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -4,7 +4,6 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  // clear any existing timers
   clearInterval(countdown);
 
   const now = Date.now();
@@ -12,14 +11,12 @@ function timer(seconds) {
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  countdown = setInterval(() => {
+  countdown = setInterval(()=>{
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
+    if(secondsLeft <= 0){
       clearInterval(countdown);
       return;
     }
-    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
@@ -27,20 +24,19 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-  document.title = display;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title=display;
   timerDisplay.textContent = display;
 }
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
-function startTimer() {
+function startTimer(){
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
 }
